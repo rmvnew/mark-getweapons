dda117a9e2d3c19ab3238c4c4c0e5ec003d15beb
Ricardo <rmvnew@gmail.com>
2025-01-17 17:27:39 -0400

    chore: added list in table

--- nui/script.js
+++ nui/script.js
@@ -1,4 +1,6 @@
 
+
+// abre a NUI depois de verificar permissao no server
 $(document).ready(()=>{
     
     window.addEventListener('message',(event)=>{
@@ -17,6 +19,7 @@ $(document).ready(()=>{
 
 })
 
+// fechar com ESC
 $(document).keyup((event)=>{
     if(event.key === 'Escape'){
 
@@ -54,3 +57,87 @@ function registerWeapon(){
 }
 
 
+
+// mostra armas na tabela
+
+$(document).ready(()=>{
+
+    const weaponsWithNames = {
+        "WEAPON_SNSPISTOL_MK2": "Pistola SNS MK2",
+        "WEAPON_PISTOL_MK2": "Pistola MK2",
+        "WEAPON_FIREWORK": "Lançador de Fogos",
+        "WEAPON_GUSENBERG": "Submetralhadora Thompson",
+        "WEAPON_PISTOL50": "Desert Eagle",
+        "WEAPON_COMBATPISTOL": "Glock",
+        "WEAPON_COMBATPDW": "PDW de Combate",
+        "WEAPON_MACHINEPISTOL": "Tec-9",
+        "WEAPON_SMG_MK2": "SMG MK2",
+        "WEAPON_SMG": "SMG",
+        "WEAPON_ASSAULTSMG": "MTAR",
+        "WEAPON_SAWNOFFSHOTGUN": "Shotgun de Cano Serrado",
+        "WEAPON_PUMPSHOTGUN_MK2": "Pump Shotgun MK2",
+        "WEAPON_ASSAULTRIFLE": "AK-47",
+        "WEAPON_ASSAULTRIFLE_MK2": "AK-47 MK2",
+        "WEAPON_HEAVYSNIPER": "Sniper Pesado",
+        "WEAPON_SPECIALCARBINE_MK2": "Carabina Especial MK2",
+        "WEAPON_DOUBLEACTION": "Revolver Double Action",
+        "WEAPON_CARBINERIFLE": "Carabina M4",
+        "WEAPON_CARBINERIFLE_MK2": "Carabina M4 MK2",
+        "WEAPON_SPECIALCARBINE": "Carabina Especial",
+        "WEAPON_STUNGUN": "Taser",
+        "WEAPON_APPISTOL": "Pistola Automática",
+        "WEAPON_ADVANCEDRIFLE": "Fuzil Avançado",
+        "WEAPON_RAYCARBINE": "Carabina Alienígena",
+        "WEAPON_BULLPUPSHOTGUN": "Bullpup Shotgun",
+        "WEAPON_DBSHOTGUN": "Shotgun de Cano Duplo",
+        "WEAPON_COMBATSHOTGUN": "Shotgun de Combate",
+        "WEAPON_RAYMINIGUN": "Minigun Alienígena",
+        "WEAPON_RPG": "Lança-Foguetes",
+        "WEAPON_HOMINGLAUNCHER": "Lança-Mísseis Teleguiados",
+        "WEAPON_COMPACTLAUNCHER": "Lançador Compacto",
+        "WEAPON_RAILGUN": "Railgun"
+    };
+    
+        const tableBody = document.querySelector('#weapons-table tbody')
+    
+        let id = 1
+    
+        for(const [key,value] of Object.entries(weaponsWithNames)){
+            const row = document.createElement('tr')
+    
+            const idCell = document.createElement('td')
+            idCell.textContent = id
+    
+            const keyCell = document.createElement('td')
+            keyCell.textContent = key
+    
+            const valueCell = document.createElement('td')
+            valueCell.textContent = value
+    
+            row.appendChild(idCell)
+            row.appendChild(keyCell)
+            row.appendChild(valueCell)
+    
+            tableBody.appendChild(row)
+            id++
+        }
+    
+
+})
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const table = document.getElementById("weapons-table")
+    const input = document.getElementById("selected_weapon")
+
+    table.addEventListener("click",(event)=>{
+        const row = event.target.closest("tr")
+        if(row && row.querySelector("td")){
+            const weaponName = row.children[1].textContent
+            input.value = weaponName
+
+            document.querySelectorAll('table tbody tr').forEach(tr => tr.classList.remove('selected'))
+
+            row.classList.add('selected')
+        }
+    })
+})
